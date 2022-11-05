fe06754bd360bf1dc367ecba1198d400b629ef7d
Sritej Tummuru <tummuru.s@etoncollege.org.uk>
2022-11-05 11:22:52 +0000

    Added function to print integers out.

--- StandardAlgorithms/main.swift
+++ StandardAlgorithms/main.swift
@@ -7,5 +7,18 @@
 
 import Foundation
 
+func printIntegers(range: Int) {
+    var currentNum = 0
+    for i in 0..<range {
+        currentNum = i
+        print(currentNum)
+    }
+}
+
 print("Hello, Sritej!")
 
+printIntegers(range: 5)
+
+let question = "How are you today"
+let message = "\(question)?"
+print(message)
